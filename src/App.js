de70cd6abacbe712e79dd409b68764fa05e2720d
Jason Lee <jason.e.lee8@gmail.com>
2022-12-18 23:52:42 -0500

    Title Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Chinatown Restaurant
         </p>
         <a
           className="App-link"
